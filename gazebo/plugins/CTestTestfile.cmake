999c0aa13bba499c9286edcbf730cf794ee6cd7c
Charles Fox <charles.fox@gmail.com>
2018-10-02 11:59:14 +0100

    rm derired files

--- gazebo/plugins/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/charles/Dropbox/podcar/gazebo/plugins
-# Build directory: /home/charles/Dropbox/podcar/gazebo/plugins
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs(gtest)
